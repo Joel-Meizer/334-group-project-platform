5e3ba8fee30a492df68dc03af82b3b2457705779
Joel Meizer <joelmeizer@outlook.com>
2024-03-29 14:29:35 +1100

    Add project files.

---
+++ Helpers/JwtService.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace _334_group_project_web_api.Helpers
+{
+    public class JwtService
+    {
+        private string secureKey = "This is a super secret key";
+        public string Generate(string id)
+        {
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secureKey));
+            var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            var header = new JwtHeader(credentials);
+            var payload = new JwtPayload(id, null, null, null, DateTime.Now.AddDays(1)); //expires in 1 day
+            var securitytoken = new JwtSecurityToken(header, payload);
+
+            return new JwtSecurityTokenHandler().WriteToken(securitytoken);
+        }
+
+        public JwtSecurityToken Verify(string jwt)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secureKey);
+            tokenHandler.ValidateToken(jwt, new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false
+            }, out SecurityToken validatedToken); ;
+
+            return (JwtSecurityToken)validatedToken;
+        }
+    }
+}
---
+++ Models/UserAccount.cs
@@ -0,0 +1,28 @@
+﻿namespace _334_group_project_web_api.Models
+{
+    public enum UserAccountType
+    {
+        Administrator,
+        ChildUser,
+        AdolescentUser,
+        AdultUser
+    }
+    public class UserAccount
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string? givenName { get; set; }
+        public string? surname { get; set; }
+        public string? displayName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public bool? EmailConfirmed { get; set; }
+        public UserAccountType? type {  get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? street { get; set; }
+        public string? city { get; set; }
+        public string? state { get; set; }
+        public string? postalCode { get; set; }
+        public string? country { get; set; }
+
+    }
+}
