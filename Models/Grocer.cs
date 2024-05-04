306ab8282ed5b2e58a8975e8ee3b45df7cd82d30
Joel-Meizer <135333517+Joel-Meizer@users.noreply.github.com>
2024-05-04 15:01:46 +1000

    Update Grocer.cs

--- Models/Grocer.cs
+++ Models/Grocer.cs
@@ -1,4 +1,4 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 
 namespace _334_group_project_web_api.Models
@@ -8,5 +8,17 @@ namespace _334_group_project_web_api.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+        public string Name { get; set; }
+        public string ContactPerson { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public List<string> ProductIds { get; set; } = new List<string>(); // List of Product Ids
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime LastUpdatedAt { get; set; }
     }
 }
